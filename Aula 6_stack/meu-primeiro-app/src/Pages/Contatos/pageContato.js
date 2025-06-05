84a0bde50510ace20fc0de93de2806fa8073929c
eltonss <eltonsarmanho@gmail.com>
2025-06-05 11:58:37 -0300

    Changes to be committed:

---
+++ Aula 6_stack/meu-primeiro-app/src/Pages/Contatos/pageContato.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function pageContato({route}) { // Ou pageContato, para manter consistência com seus outros arquivos
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Página de Contato</Text>
+      <Text style={styles.text}>Nome: {route.params?.nome}</Text>
+      <Text style={styles.text}>Documento: {route.params?.documento}</Text>  
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  ButtonStyle: {
+    backgroundColor: 'skyblue',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  }
+
+});
